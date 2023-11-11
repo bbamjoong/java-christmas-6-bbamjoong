1a22e2deee48398205b46c8a00a44bc028cf7944
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-11-11 21:45:53 +0900

    test(FoodsTest): Food의 리스트를 입력값으로 가지는 Foods 클래스 테스트 기능 개발
    - 메뉴들의 개수 합이 20개가 넘으면 예외가 발생하는 테스트 기능 개발

---
+++ src/test/java/christmas/domain/FoodsTest.java
@@ -0,0 +1,41 @@
+package christmas.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import christmas.exceptions.InvalidOrderException;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FoodsTest {
+    List<Food> validFoods;
+    List<Food> invalidFoods;
+
+    @BeforeEach
+    void setUp() {
+        validFoods = new ArrayList<>();
+        validFoods.add(Food.of("양송이수프", "10"));
+        validFoods.add(Food.of("제로콜라", "10"));
+
+        invalidFoods = new ArrayList<>();
+        invalidFoods.add(Food.of("양송이수프", "10"));
+        invalidFoods.add(Food.of("제로콜라", "20"));
+    }
+
+    @Test
+    @DisplayName("주문 개수가 20이 넘으면 예외 발생")
+    void overThresholdCountTest() {
+        assertThatThrownBy(() -> Foods.of(invalidFoods))
+                .isInstanceOf(InvalidOrderException.class);
+    }
+
+    @Test
+    @DisplayName("주문 개수가 20이 넘지 않으면 정상 작동")
+    void correctCountTest() {
+        assertThatCode(() -> Foods.of(validFoods))
+                .doesNotThrowAnyException();
+    }
+}
