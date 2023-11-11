0dba8207151ee39306d96be548faa95cf06beb0a
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-11-11 23:20:09 +0900

    feat(VisitDate): 방문 날짜 검증 Record 기능 개발
    - Constraints : START_DATE(1), END_DATE(31) 추가

--- src/main/java/christmas/domain/Constraints.java
+++ src/main/java/christmas/domain/Constraints.java
@@ -2,8 +2,9 @@ package christmas.domain;
 
 public enum Constraints {
     COUNT_THRESHOLD(20),
-    PAIR_SIZE(2);
-
+    PAIR_SIZE(2),
+    START_DATE(1),
+    END_DATE(31);
     private final int value;
 
     Constraints(int value) {
---
+++ src/main/java/christmas/domain/VisitDate.java
@@ -0,0 +1,36 @@
+package christmas.domain;
+
+import static christmas.domain.Constraints.END_DATE;
+import static christmas.domain.Constraints.START_DATE;
+
+import christmas.exceptions.InvalidDateException;
+
+public record VisitDate(int date) {
+
+    public static VisitDate of(String date) {
+        int dateValue = validateVisitDate(date);
+        return new VisitDate(dateValue);
+    }
+
+    private static int validateVisitDate(String date) {
+        int value = inputToNumber(date);
+        validateDateRange(value);
+        return value;
+    }
+
+    // 정수형으로 입력되지 않으면 예외 발생
+    private static int inputToNumber(String date) {
+        try {
+            return Integer.parseInt(date);
+        } catch (NumberFormatException e) {
+            throw new InvalidDateException();
+        }
+    }
+
+    // 1 이상 31 이하가 아닐 경우 예외 발생
+    private static void validateDateRange(int value) {
+        if (value < START_DATE.getValue() || value > END_DATE.getValue()) {
+            throw new InvalidDateException();
+        }
+    }
+}
