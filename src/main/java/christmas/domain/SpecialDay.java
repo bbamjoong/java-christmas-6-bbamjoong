e13e0b9f6c768e1085bbeebd9d1842665524bf27
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-11-12 20:44:52 +0900

    feat(SpecialDay): 특별 할인 날짜를 관리하는 Enum 개발

---
+++ src/main/java/christmas/domain/SpecialDay.java
@@ -0,0 +1,24 @@
+package christmas.domain;
+
+public enum SpecialDay {
+    NOVEMBER_3(3),
+    NOVEMBER_10(10),
+    NOVEMBER_17(17),
+    NOVEMBER_24(24),
+    NOVEMBER_25(25),
+    NOVEMBER_31(31);
+
+    private final int eventDay;
+
+    SpecialDay(int eventDay) {
+        this.eventDay = eventDay;
+    }
+
+    public int getValue() {
+        return eventDay;
+    }
+
+    public boolean isSpecialDay(int visitDay) {
+        return visitDay == this.eventDay;
+    }
+}
