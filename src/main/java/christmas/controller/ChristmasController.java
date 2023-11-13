8a0f59b8efab68319d494f168a4248203ec9507c
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-11-13 15:05:52 +0900

    feat: 주문 메뉴 출력 기능 구현
    - Controller: OutputView의 메뉴 출력 메소드 호출
    - OutputView: Foods를 입력값으로 받아 각각의 음식 별 이름, 개수를 출력하는 기능 개발
    - ViewMessage: 주문 메뉴 출력 메시지 추가

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -17,8 +17,8 @@ public class ChristmasController {
     public void run() {
         VisitDate visitDate = getValidVisitDate();
         Foods foods = getValidFoods();
-        OutputView.printPreviewEventBenefits();
         printPreviewBenefitsMessage();
+        printOrderMenu(foods);
     }
 
     // 방문 날짜 객체 생성
@@ -44,6 +44,11 @@ public class ChristmasController {
     // 이벤트 혜택 미리보기 메시지 출력
     private void printPreviewBenefitsMessage() {
         OutputView.printPreviewEventBenefits();
+    }
+
+    // 주문 메뉴 출력
+    private void printOrderMenu(Foods foods) {
+        OutputView.printFoods(foods);
     }
 
     // 함수형 인터페이스
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,9 +1,22 @@
 package christmas.view;
 
+import static christmas.view.ViewMessage.FOOD_PRESET;
+import static christmas.view.ViewMessage.ORDER_MENU;
 import static christmas.view.ViewMessage.PREVIEW_EVENT_BENEFITS;
+
+import christmas.domain.Foods;
 
 public class OutputView {
     public static void printPreviewEventBenefits() {
         System.out.println(PREVIEW_EVENT_BENEFITS.getMessage());
+    }
+
+    public static void printFoods(Foods foods) {
+        System.out.println(ORDER_MENU.getMessage());
+        foods.orderFoods().forEach(orderFood -> printSingleFood(orderFood.name(), orderFood.count()));
+    }
+
+    private static void printSingleFood(String name, int count) {
+        System.out.printf(FOOD_PRESET.getMessage(), name, count);
     }
 }
