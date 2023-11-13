f2617f430943d94fffe41c6c3e66b5a0f2f1964b
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-11-14 00:02:18 +0900

    refactor(ChristmasController): 메소드 분리

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,5 +1,6 @@
 package christmas.controller;
 
+import christmas.domain.Constraints;
 import christmas.domain.DiscountCalculator;
 import christmas.domain.DiscountType;
 import christmas.domain.Foods;
@@ -12,55 +13,42 @@ import java.util.function.Supplier;
 
 public class ChristmasController {
     private final ChristmasService christmasService;
+    private VisitDate visitDate;
+    private Foods foods;
 
     public ChristmasController() {
         this.christmasService = new ChristmasService();
     }
 
     public void run() {
-        VisitDate visitDate = getValidVisitDate();
-        Foods foods = getValidFoods();
+        handleDecemberEvent();
+
+        int totalPrice = calculateTotalPrice();
+        Map<DiscountType, Integer> discountsInfo = calculateDiscountsInfo();
+        int finalPrice = calculateFinalPrice(totalPrice, discountsInfo);
+
+        printEventDetails(totalPrice, discountsInfo, finalPrice);
+    }
+
+    // 12월 이벤트 처리
+    private void handleDecemberEvent() {
+        visitDate = getValidVisitDate();
+        foods = getValidFoods();
         printPreviewBenefitsMessage();
         printOrderMenu(foods);
-
-        int totalPrice = foods.calculateTotalPrice();
-        printTotalPriceBeforeDiscount(totalPrice);
-        printFreeGift(totalPrice);
-
-        DiscountCalculator discountCalculator = new DiscountCalculator(foods, visitDate.date());
-        Map<DiscountType, Integer> discountsInfo = christmasService.calculateDiscountsMap(discountCalculator);
-        int discounts = christmasService.calculateDiscounts(discountsInfo);
-        printDiscountsInformation(discountsInfo, discounts);
-
-        printDiscountsPrice(discounts);
-
-        int discountsExceptFreeGift = christmasService.calculateDiscountExceptFreeGift(discountsInfo);
-        int finalPrice = totalPrice - discountsExceptFreeGift;
-        printFinalPrice(finalPrice);
-        printBadge(discountsExceptFreeGift);
     }
 
-    // 방문 날짜 객체 생성
+    // 유효한 방문 날짜 가져오기
     private VisitDate getValidVisitDate() {
-        return getValidInput(this::createVisitDate);
+        return getValidInput(() -> christmasService.getVisitDate(InputView.printVisitDate()));
     }
 
-    private VisitDate createVisitDate() {
-        String input = InputView.printVisitDate();
-        return christmasService.getVisitDate(input);
+    // 유효한 음식 가져오기
+    private Foods getValidFoods() {
+        return getValidInput(() -> christmasService.getFoods(InputView.printMenu()));
     }
 
-    // Foods 객체 생성
-    public Foods getValidFoods() {
-        return getValidInput(this::createFoods);
-    }
-
-    private Foods createFoods() {
-        String input = InputView.printMenu();
-        return christmasService.getFoods(input);
-    }
-
-    // 이벤트 혜택 미리보기 메시지 출력
+    // 혜택 미리보기 메시지 출력
     private void printPreviewBenefitsMessage() {
         OutputView.printPreviewEventBenefits();
     }
@@ -70,41 +58,92 @@ public class ChristmasController {
         OutputView.printFoods(foods);
     }
 
-    // 할인 전 총주문 금액 출력
+    // 총 주문 가격 계산
+    private int calculateTotalPrice() {
+        return foods.calculateTotalPrice();
+    }
+
+    // 할인 정보 계산
+    private Map<DiscountType, Integer> calculateDiscountsInfo() {
+        return christmasService.calculateDiscountsMap(
+                DiscountCalculator.of(foods, visitDate.date())
+        );
+    }
+
+    // 총 할인 금액 계산
+    private int calculateTotalDiscounts(Map<DiscountType, Integer> discountsInfo) {
+        return christmasService.calculateDiscounts(discountsInfo);
+    }
+
+    // 최종 결제 예정 가격 계산
+    private int calculateFinalPrice(int totalPrice, Map<DiscountType, Integer> discountsInfo) {
+        int discountsExceptFreeGift = christmasService.calculateDiscountExceptFreeGift(discountsInfo);
+        return totalPrice - discountsExceptFreeGift;
+    }
+
+    // 이벤트 세부 정보 출력
+    private void printEventDetails(int totalPrice, Map<DiscountType, Integer> discountsInfo, int finalPrice) {
+        int totalDiscounts = calculateTotalDiscounts(discountsInfo);
+
+        printEventBenefits(totalPrice);
+        printEventResult(finalPrice, totalDiscounts);
+        printDiscountInformation(discountsInfo, totalDiscounts);
+    }
+
+    // 이벤트 혜택 출력
+    private void printEventBenefits(int totalPrice) {
+        printTotalPriceBeforeDiscount(totalPrice);
+        printFreeGift(totalPrice);
+    }
+
+    // 할인 정보 출력
+    private void printDiscountInformation(Map<DiscountType, Integer> discountsInfo, int totalDiscounts) {
+        printDiscountsInfo(discountsInfo, totalDiscounts);
+        printDiscountsPrice(totalDiscounts);
+    }
+
+    // 최종 가격 출력
+    private void printEventResult(int finalPrice, int totalDiscounts) {
+        printFinalPrice(finalPrice);
+        printBadge(totalDiscounts);
+    }
+
+    // 할인 전 총 가격 출력
     private void printTotalPriceBeforeDiscount(int totalPrice) {
         OutputView.printPrice(totalPrice);
     }
 
-    // 증정 메뉴 출력
+    // 증정품 출력
     private void printFreeGift(int totalPrice) {
-        Boolean freeGift = christmasService.findFreeGift(totalPrice);
+        boolean freeGift = christmasService.findFreeGift(totalPrice);
         OutputView.printGiftMenu(freeGift);
     }
 
-    // 혜택 내역 출력
-    private void printDiscountsInformation(Map<DiscountType, Integer> discountsInfo, int discounts) {
-        if (discounts == 0) {
+    // 할인 정보 출력
+    private void printDiscountsInfo(Map<DiscountType, Integer> discountsInfo, int totalDiscounts) {
+        if (totalDiscounts == Constraints.ZERO.getValue()) {
             OutputView.printNoDiscounts();
             return;
         }
         OutputView.printDiscounts(discountsInfo);
     }
 
-    // 총혜택 금액 출력
-    private void printDiscountsPrice(int discounts) {
-        OutputView.printDiscountsAmount(discounts);
+    // 할인 금액 출력
+    private void printDiscountsPrice(int totalDiscounts) {
+        OutputView.printDiscountsAmount(totalDiscounts);
     }
 
+    // 최종 결제 금액 출력
     private void printFinalPrice(int finalPrice) {
         OutputView.printFinalAmount(finalPrice);
     }
 
+    // 배지 출력
     private void printBadge(int finalPrice) {
         String badgeName = christmasService.getBadge(finalPrice);
         OutputView.printBadgeName(badgeName);
     }
 
-    // 함수형 인터페이스
     private <T> T getValidInput(Supplier<T> method) {
         while (true) {
             try {
